254b5b0a4f457a6a382e73b270c5e29598e82058
hanshhilar <hanshhilar705@gmail.com>
2026-03-22 07:49:19 +0000

    added qr code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import qrcode
+from io import BytesIO
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🔗 QR Code Generator")
+
+# The URL input
+link = st.text_input("Enter your URL:", "https://hanshhilar705.wixsite.com/qrconect")
+
+if link:
+    qr = qrcode.make(link)
+    buf = BytesIO()
+    qr.save(buf, format="PNG")
+    st.image(buf.getvalue(), width=300)
+    st.download_button("Download QR", buf.getvalue(), "qr.png", "image/png")
